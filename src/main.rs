0790ff8ab9d554cd57f308bd66639ff5ee00e270
DevinBayly <baylyd@email.arizona.edu>
2021-04-06 20:16:09 -0700

    got points to be added, now need to allow for subdivide to re-allocate its contents

--- src/main.rs
+++ src/main.rs
@@ -1,122 +1,179 @@
+use rand::prelude::*;
+
+
 #[derive(Debug)]
 enum EL {
     None,
     Some(Box<QT>),
 }
-#[derive(Debug)]
+#[derive(Debug,Clone,Copy)]
 struct PT {
-    x:f32,
-    y:f32
-
+    x: f32,
+    y: f32,
 }
 impl PT {
-    fn new(x:f32,y:f32) -> Self {
-        PT {
-            x,y
-        }
+    fn new(x: f32, y: f32) -> Self {
+        PT { x, y }
     }
 }
 
 #[derive(Debug)]
-struct Rect{
-    top:f32,
-    bottom:f32,
-    right:f32,
-    left:f32,
-    center:PT,
-    w:f32,
-    h:f32
+struct Rect {
+    top: f32,
+    bottom: f32,
+    right: f32,
+    left: f32,
+    center: PT,
+    w: f32,
+    h: f32,
 }
 // note y is up!
 impl Rect {
-    fn new(center:PT,w:f32,h:f32) ->Self {
-        Rect{
-            top: center.y + h/2.0,
-            bottom:center.y - h/2.0,
-            right: center.x + w/2.0,
-            left : center.x - w/2.0,
+    fn new(center: PT, w: f32, h: f32) -> Self {
+        Rect {
+            top: center.y + h / 2.0,
+            bottom: center.y - h / 2.0,
+            right: center.x + w / 2.0,
+            left: center.x - w / 2.0,
             center,
             w,
             h,
         }
     }
-    fn contains(&self,other:&PT) -> bool{
+    fn contains(&self, other: &PT) -> bool {
         other.x >= self.left && other.x < self.right && other.y >= self.bottom && other.y < self.top
     }
 }
 #[derive(Debug)]
 struct QT {
-    value: Vec<f32>,
-    capacity:usize,
-    bb:Rect,
-    subdiv:bool,
-    child: EL,
+    value: Vec<PT>,
+    capacity: usize,
+    bb: Rect,
+    subdiv: bool,
+    // children organized by cardinal points
+    ne_child: EL,
+    nw_child: EL,
+    sw_child: EL,
+    se_child: EL,
 }
 
 impl QT {
-    fn addPt(&mut self, other: f32) {
+    fn new(rect: Rect, cap: usize) -> Self {
+        //
+        Self {
+            bb: rect,
+            capacity: cap,
+            subdiv: false,
+            value: vec![],
+            ne_child: EL::None,
+            nw_child: EL::None,
+            sw_child: EL::None,
+            se_child: EL::None,
+        }
+    }
+    fn addPt(&mut self, other: PT) {
+        // if it doesn't contain the point don't do anything
+        if !self.bb.contains(&other) {
+            return 
+        }
         // use logic to decide if we should push or punt
         if self.value.len() < self.capacity {
             self.value.push(other);
         } else {
             if !self.subdiv {
-                self.grow();
+                self.subdivide();
             }
-            // go to the next child
-            let mut ref_child = &mut self.child;
-            match ref_child {
-                EL::Some(b) => {
-                    // change ref_child to a new child
-                    b.addPt(other);
-                }
-                _ => {
-                }
-            }
+            // go through the children and add the point to the correct oneto the next child
+            // ne
+            let mut ref_child = &mut self.ne_child;
+            Self::add_to_child(ref_child, other);
+            // nw
+            let mut ref_child = &mut self.nw_child;
+            Self::add_to_child(ref_child, other);
+            // se
+            let mut ref_child = &mut self.se_child;
+            Self::add_to_child(ref_child, other);
+            // sw
+            let mut ref_child = &mut self.sw_child;
+            Self::add_to_child(ref_child, other);
+            //match ref_child {
+            //    EL::Some(b) => {
+            //        // change ref_child to a new child
+            //        b.addPt(other);
+            //    }
+            //    _ => {
+            //    }
+            //}
         }
     }
-    fn grow(&mut self) {
+    fn add_to_child(child: &mut EL, other: PT) {
+        match child {
+            EL::Some(b) => b.addPt(other),
+            _ => {}
+        };
+    }
+    fn subdivide(&mut self) {
         // add another element at the end of the chain
-        let mut ref_child = &mut self.child;
-        let mut end_loop = false;
+        // in subdivide we should populate all the cardinal children we don't have to loop because it will get called on the correct unsubdivided element anyways
         self.subdiv = true;
-        loop {
-
-            match ref_child {
-                EL::Some(b) => {
-                    // change ref_child to a new child
-                    ref_child = &mut b.child;
-                }
-                _ => {
-                    *ref_child = EL::Some(Box::new(QT {
-                        capacity:4,
-                        subdiv:false,
-                        bb:Rect::new(PT::new(self.bb.center.x - self.bb.w/4.0,self.bb.center.y),self.bb.w/2.0,self.bb.h),
-                        value: vec![self.value[0] + 1f32],
-                        child: EL::None,
-                    }));
-                    end_loop = true;
-                }
-            }
-            if end_loop {
-                break;
-            }
-        }
+        self.ne_child = EL::Some(Box::new(QT::new(
+            Rect::new(
+                PT::new(
+                    self.bb.center.x - self.bb.w / 4.0,
+                    self.bb.center.y + self.bb.h / 4.0,
+                ),
+                self.bb.w / 2.0,
+                self.bb.h / 2.0,
+            ),
+            4,
+        )));
+        self.nw_child = EL::Some(Box::new(QT::new(
+            Rect::new(
+                PT::new(
+                    self.bb.center.x + self.bb.w / 4.0,
+                    self.bb.center.y + self.bb.h / 4.0,
+                ),
+                self.bb.w / 2.0,
+                self.bb.h / 2.0,
+            ),
+            4,
+        )));
+        self.se_child = EL::Some(Box::new(QT::new(
+            Rect::new(
+                PT::new(
+                    self.bb.center.x - self.bb.w / 4.0,
+                    self.bb.center.y - self.bb.h / 4.0,
+                ),
+                self.bb.w / 2.0,
+                self.bb.h / 2.0,
+            ),
+            4,
+        )));
+        self.sw_child = EL::Some(Box::new(QT::new(
+            Rect::new(
+                PT::new(
+                    self.bb.center.x + self.bb.w / 4.0,
+                    self.bb.center.y - self.bb.h / 4.0,
+                ),
+                self.bb.w / 2.0,
+                self.bb.h / 2.0,
+            ),
+            4,
+        )));
     }
 }
 
 fn main() {
     let width = 200.0;
     let height = 200.0;
-    let mut t = QT {
-        bb:Rect::new(PT::new(width/2.0,height/2.0),width,height),
-        capacity:4,
-        subdiv:false,
-        value: vec![0f32],
-        child: EL::None,
-    };
+    // make a new quad tree
+    // t
+    let mut t = QT::new(Rect::new(PT::new(100.0,100.0),200.0,200.0),4);
+    let mut rng = thread_rng();
     for i in 0..16 {
-        println!("i is {}",i);
-        t.addPt(i as f32);
+        let pt = PT::new(rng.gen::<f32>(),rng.gen::<f32>());
+        println!("i is {}", i);
+        t.addPt(pt);
     }
-    println!("{:#?}",t);    
+    println!("{:#?}", t);
 }
